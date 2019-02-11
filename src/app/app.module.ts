0d0228bcda867c178f3520ed3d28c0c135257ce1
justin clappsy <justinc9877@gmail.com>
2019-02-11 14:06:16 -0500

    Added httpclientmodule to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { TestPageComponent } from './test-page/test-page.component';
 
 @NgModule({
-	declarations: [ AppComponent, HeaderComponent, FooterComponent, TestPageComponent ],
-	imports: [ BrowserModule ],
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		FooterComponent,
+		TestPageComponent
+	],
+	imports: [ BrowserModule, HttpClientModule ],
 	providers: [],
 	bootstrap: [ AppComponent ]
 })
