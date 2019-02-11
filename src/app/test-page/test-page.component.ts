593cea5befdcf11836f34c26fcd741736eda1d14
justin clappsy <justinc9877@gmail.com>
2019-02-11 14:06:25 -0500

    Added content to test-page

--- src/app/test-page/test-page.component.ts
+++ src/app/test-page/test-page.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { StockService } from '../stock.service';
+import { Stock } from '../stocks';
 @Component({
-  selector: 'app-test-page',
-  templateUrl: './test-page.component.html',
-  styleUrls: ['./test-page.component.css']
+	selector: 'app-test-page',
+	templateUrl: './test-page.component.html',
+	styleUrls: [ './test-page.component.css' ]
 })
 export class TestPageComponent implements OnInit {
+	public stocks?: Stock[] = [];
+	public stock: Stock;
 
-  constructor() { }
+	constructor(public stockService: StockService) {
+		this.stock = {
+			id: 1,
+			name: 'test',
+			price: 500
+		};
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.getTestStock();
+		// this.getStocks();
+	}
+	getStocks() {
+		this.stockService.getUser({}).subscribe((stock) => {
+			this.stocks = stock;
+			console.log('Stock: ', stock);
+		});
+	}
 
+	getTestStock() {
+		console.log(this.stock);
+		return this.stock;
+	}
 }
