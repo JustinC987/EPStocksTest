43eff6fc62ad1b403a721a122a64c54aa1d5fd23
justin clappsy <justinc9877@gmail.com>
2019-02-11 13:19:05 -0500

    Added imports

--- src/app/stock.service.ts
+++ src/app/stock.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { catchError, tap, map } from 'rxjs/operators';
 
+import { Observable } from 'rxjs';
+import { HandlerService } from './handler.service';
+import { AuthService } from '../app/auth.service';
+import { Stock } from './stocks';
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class StockService {
-
-  constructor() { }
+	constructor() {}
 }
