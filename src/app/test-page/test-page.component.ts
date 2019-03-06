333fdc78bc0654f0130cfb39d5817ef5fc86940c
justin clappsy <justinc9877@gmail.com>
2019-03-06 10:26:30 -0500

    Get Data from json server

--- src/app/test-page/test-page.component.ts
+++ src/app/test-page/test-page.component.ts
@@ -10,22 +10,10 @@ export class TestPageComponent implements OnInit {
 	public stocks?: Stock[] = [];
 	public stock: Stock;
 
-	constructor(public stockService: StockService) {
-		/* this is mock data. This is the stock object being displayed in the ul in the html 
-    //where the div is <div *ngIf="stock"</div>.
-    let stock of stocks is for when you use this.getStocks();
-    */
-		this.stock = {
-			id: 1,
-			name: 'test',
-			price: 500
-		};
-	}
+	constructor(public stockService: StockService) {}
 
 	ngOnInit() {
-		this.getTestStock();
-		// use the function below once you have an endpoint
-		// this.getStocks();
+		this.getStocks();
 	}
 	getStocks() {
 		this.stockService.getUser({}).subscribe((stock) => {
